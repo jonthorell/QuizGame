9125430f2b57545516d97afa10d3655b30f0d035
jonthorell <jthorell@gmail.com>
2023-02-08 10:57:12 +0000

    Add: modal with game-rules added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,32 @@ const gameTitle = "the music quiz master";
 const maxQuestions = 10;
 const professor = "Professor Elvin Atombender thinks you won't escape.\nBonuspoint if you know where the quote comes from!";
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     let welcomeEvent = document.getElementById('welcome');
     welcomeEvent.addEventListener('mouseover', function () {
